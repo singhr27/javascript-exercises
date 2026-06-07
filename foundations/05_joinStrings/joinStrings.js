defc44519c4af6bfb63cb390e90cb59b8720348e
Ravdeep Singh <144287500+singhr27@users.noreply.github.com>
2026-06-07 16:27:00 +0530

    complete problems 01-05

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -1,15 +1,14 @@
-/*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
+
+let firstName= "Carlos";
+let lastName= "Stevenson";
+let thisYear= 1965;
+let birthYear= 1947;
+let age= thisYear - birthYear;
+let greeting= `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`;
+let fullName= firstName + " " + lastName;
 
 // Add your code right below, good luck!
-
-
 
 
 
